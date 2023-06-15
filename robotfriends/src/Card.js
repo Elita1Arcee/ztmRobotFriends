685fa331936fcd72d2ff9de8b42e54dd1847eaa2
Louisa Blackshear-Jones <louisa@louisajones.dev>
2023-06-14 23:13:06 -0400

    setting up props for Card component

--- robotfriends/src/Card.js
+++ robotfriends/src/Card.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
+
 const Card = () =>{
     return (
         <div className= "bg-light-green dib br3 pa3 ma2 grow bw-2 shadow-5">
             <img src="https://robohash.org/arcee?200x200" alt="image of random robot"/>
             <div>
-                <h2>Lauryn Lynn</h2>
-                <p>thereallboogie@gmail.com</p>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
             </div>
         </div>
     );
--- robotfriends/src/index.js
+++ robotfriends/src/index.js
@@ -4,14 +4,15 @@ import './index.css';
 import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import {robots} from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      <Card />
-      <Card />
-      <Card />
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
     </div>
     
   </React.StrictMode>
