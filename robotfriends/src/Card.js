963647c77d2d3d2b7d5c8c2168f5249be351dfa7
Louisa Blackshear-Jones <louisa@louisajones.dev>
2023-06-14 22:43:35 -0400

    added Card component

---
+++ robotfriends/src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () =>{
+    return (
+        <div>
+            <img src="" alt="image of random robot"/>
+            <div>
+                <h2>Lauryn Lynn</h2>
+                <p>thereallboogie@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card
