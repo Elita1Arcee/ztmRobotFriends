6b2df04dad92d5cff97d11f19af50b90d96b9107
Louisa Blackshear-Jones <louisa@louisajones.dev>
2023-06-14 23:20:55 -0400

    css stying

--- robotfriends/src/Card.js
+++ robotfriends/src/Card.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Card = () =>{
     return (
-        <div className= "bg-light-green dib br3 pa3 ma2 grow bw-2 shadow-5">
-            <img src="https://robohash.org/arcee?200x200" alt="image of random robot"/>
+        <div className= "tc bg-light-green dib br3 pa3 ma2 grow bw-2 shadow-5">
+            <img src={`https://robohash.org/${props.id}?200x200`} alt="image of random robot"/>
             <div>
                 <h2>{props.name}</h2>
                 <p>{props.email}</p>
