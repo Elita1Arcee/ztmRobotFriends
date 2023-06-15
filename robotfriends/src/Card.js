80896cfeca38c71e125bbecb407a17149544e7f7
Louisa Blackshear-Jones <louisa@louisajones.dev>
2023-06-14 22:56:34 -0400

    adding tachyon styling and robo api

--- robotfriends/src/Card.js
+++ robotfriends/src/Card.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Card = () =>{
     return (
-        <div>
-            <img src="" alt="image of random robot"/>
+        <div className= "bg-light-green dib br3 pa3 ma2 grow">
+            <img src="https://robohash.org/arcee?200x200" alt="image of random robot"/>
             <div>
                 <h2>Lauryn Lynn</h2>
                 <p>thereallboogie@gmail.com</p>
--- robotfriends/src/index.js
+++ robotfriends/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <Card />
+      <Card />
+      <Card />
+    </div>
+    
   </React.StrictMode>
 );
 
