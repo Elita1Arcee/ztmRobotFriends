8881c0a3c8a1c1ceedbca2508a0c2a2dc02281af
Louisa Blackshear-Jones <louisa@louisajones.dev>
2023-06-19 11:41:05 -0400

    updated packages, fixed props

--- robotfriends/src/Card.js
+++ robotfriends/src/Card.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-const Card = () =>{
+const Card = (props) =>{
     return (
         <div className= "tc bg-light-green dib br3 pa3 ma2 grow bw-2 shadow-5">
             <img src={`https://robohash.org/${props.id}?200x200`} alt="image of random robot"/>
