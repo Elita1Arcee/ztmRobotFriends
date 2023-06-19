d8703c6b9f9cb006b68f7c3484128e2cdb3845e9
Louisa Blackshear-Jones <louisa@louisajones.dev>
2023-06-19 18:36:11 -0400

    Reworking code into cleaner units

--- robotfriends/src/Card.js
+++ robotfriends/src/Card.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-//Receiving props and destructurin props
-const Card = ({ name, email, id}) =>{
+//Receiving props and destructuring props
+const Card = ({ name, email, id, username}) =>{
     
     return (
         <div className= "tc bg-light-green dib br3 pa3 ma2 grow bw-2 shadow-5">
-            <img src={`https://robohash.org/${id}?200x200`} alt="image of random robot"/>
+            <img src={`https://robohash.org/${username}?200x200`} alt="image of random robot"/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
--- robotfriends/src/index.js
+++ robotfriends/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card';
+import Robotlist from './Robotlist';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import {robots} from './robots';
@@ -10,9 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+      {/* accepting a robots array as a prop */}
+      <Robotlist robots = {robots}/>
     </div>
     
   </React.StrictMode>
